30b9ca475156db085ccc0b1614f3a268d0064b37
Daniel Bahamón <mr.danielbahamon@gmail.com>
2020-07-23 19:56:58 -0500

    23 07 2020 16

--- app/models/mock.rb
+++ app/models/mock.rb
@@ -28,8 +28,7 @@ class Mock < ApplicationRecord
 
 	# For movie
 	has_attached_file :movie, styles: {
-		medium: {geometry: "640x480#", format: 'mp4'},
-		thumb: {geometry: "100x50", format: 'jpg', time: 10}
+		medium: {geometry: "640x480#", format: 'mp4'}
 	}, proccessor: [:transcoder]
 	
 
