aac910386826ead6147e7f7aa9a2b2b60f76d545
Daniel Bahamón <mr.danielbahamon@gmail.com>
2020-08-18 21:21:49 -0500

    18 08 2020 12

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200818235308) do
+ActiveRecord::Schema.define(version: 20200818234936) do
 
   create_table "answers", force: :cascade do |t|
     t.text "comment"
