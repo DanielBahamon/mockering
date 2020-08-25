48ba1a03e90feb6e0dd777868440d1273c45af55
Daniel Bahamón <mr.danielbahamon@gmail.com>
2020-08-25 13:49:25 -0500

    25 08 2020 3

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,8 @@
 class Group < ActiveRecord::Base
   groupify :group
+  before_validation :set_uuid, on: :create
+
+  def set_uuid
+    self.id = SecureRandom.uuid
+  end
 end
--- app/models/group_membership.rb
+++ app/models/group_membership.rb
@@ -1,3 +1,8 @@
 class GroupMembership < ActiveRecord::Base
   groupify :group_membership
+  before_validation :set_uuid, on: :create
+
+  def set_uuid
+    self.id = SecureRandom.uuid
+  end
 end
