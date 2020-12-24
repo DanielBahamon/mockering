348616fe0018659c7545fb85d730ff1ddc20a66c
Daniel Bahamón <mr.danielbahamon@gmail.com>
2020-12-24 10:14:29 -0500

    24 12 2020 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,14 @@ class PagesController < ApplicationController
 	    @q = Mock.ransack(params[:q])
     	@mocks = @q.result(distinct: true).order("created_at DESC").paginate(page: params[:page], per_page: 30).where(privated: false, reported: false)
 		# @mocks = @q.result(distinct: true).order("created_at DESC").paginate(page: params[:page], per_page: 20)
+
+		if params[:tag].present?
+			@mocks = Mock.tagged_with(params[:tag])
+			.paginate(page: params[:page], per_page: 4)
+			.where(privated: false, reported: false, unlist: false)
+		else
+			@mocks = @q.result(distinct: true).order("created_at DESC").paginate(page: params[:page], per_page: 30).where(privated: false, reported: false)
+		end
 	end
 
 	def policy
