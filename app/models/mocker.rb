cfc23e4d71dd3d2c6abcc8348f242523758f8a46
Daniel Bahamón <mr.danielbahamon@gmail.com>
2021-03-07 19:20:13 -0500

    07 03 2021 2

--- app/models/mocker.rb
+++ app/models/mocker.rb
@@ -96,9 +96,6 @@ class Mocker < ApplicationRecord
 
   	has_one :setting
   	after_create :add_setting
-
-  	
-  	ransack_alias :mocker, :mocker_first_name_or_mocker_last_name_or_mocker_slug
   	
 
 	  def add_setting
