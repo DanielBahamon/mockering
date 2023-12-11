6e4600622a1fcb2940098cf792fb528fb68d109c
bahamon <mr.danielbahamon@gmail.com>
2023-12-11 01:18:36 -0500

    DEPLOY DECEMBER 11, 2023 I

--- db/migrate/20231211060607_change_mocktype_to_integer_in_mocks_using.rb
+++ db/migrate/20231211060607_change_mocktype_to_integer_in_mocks_using.rb
@@ -1,5 +1,14 @@
 class ChangeMocktypeToIntegerInMocksUsing < ActiveRecord::Migration[6.1]
-  def change
-    change_column :mocks, :mocktype, 'integer USING CAST(mocktype AS integer)'
+  def up
+    # SQLite no admite directamente el cambio de tipo de columna, así que usamos un enfoque diferente.
+    change_column :mocks, :mocktype, :integer
+
+    # Luego, eliminamos los valores no enteros de la columna si los hubiera.
+    execute("UPDATE mocks SET mocktype = NULL WHERE NOT (mocktype = '' OR mocktype = '0' OR mocktype = '1');")
+  end
+
+  def down
+    # Si necesitas revertir esta migración, simplemente cambia el tipo de columna a string.
+    change_column :mocks, :mocktype, :integer
   end
 end
