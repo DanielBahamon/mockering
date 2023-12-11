af71829331682436ca9e7b0b217cca17936c5fcc
bahamon <mr.danielbahamon@gmail.com>
2023-12-11 00:54:30 -0500

    new deploy 3

--- db/migrate/20231211054109_change_mocktype_to_integer_in_mocks.rb
+++ db/migrate/20231211054109_change_mocktype_to_integer_in_mocks.rb
@@ -1,9 +1,5 @@
 class ChangeMocktypeToIntegerInMocks < ActiveRecord::Migration[6.1]
-  def up
+  def change
     change_column :mocks, :mocktype, :integer
-  end
-  
-  def down
-    change_column :mocks, :mocktype, :integer # Cambia :integer a :string si el rollback es necesario
   end
 end
